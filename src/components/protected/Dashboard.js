c95e7d14b0d014ac6950fd8d2b91c7b68c7c6458
Klayton <kliekkio@gmail.com>
2017-10-25 11:34:28 -0700

    working react firebase auth navbar

---
+++ src/components/protected/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+  render () {
+    return (
+      <div>
+        Pick-A-Flick goes here, can only be seen when signed in.
+      </div>
+    )
+  }
+}
